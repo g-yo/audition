7e845b1ce6fa649d35230e35f967a629a2b988b7
g-yo <geoaugustion@gmail.com>
2024-08-05 12:01:38 +0530

    apply

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-apt-get update
-apt-get install -y python3-pip
-pip3 install -r /var/www/html/auditions/requirements.txt
+sudo yum update -y
+sudo yum install -y git
+sudo yum groupinstall -y "Development Tools"
+sudo yum install -y openssl-devel bzip2-devel libffi-devel
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tgz
+sudo tar xzf Python-3.12.0.tgz
+cd Python-3.12.0
+sudo ./configure --enable-optimizations
+sudo make altinstall
+sudo ln -s /usr/local/bin/python3.12 /usr/bin/python3.12
+sudo ln -s /usr/local/bin/pip3.12 /usr/bin/pip3.12
+cd /var/www/html/auditions
+pip3.12 install -r requirements.txt
