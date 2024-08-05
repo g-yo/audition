26b2e03e097c22cabc8a2fe10f168b54eec5c36a
g-yo <geoaugustion@gmail.com>
2024-08-05 12:34:02 +0530

    apply

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-cd /var/www/html/auditions
-python3 manage.py migrate
+set -e
+
+# Navigate to the project directory
+PROJECT_DIR="/home/ec2-user/auditions"
+if [ -d "$PROJECT_DIR" ]; then
+    cd "$PROJECT_DIR"
+else
+    echo "Directory $PROJECT_DIR does not exist."
+    exit 1
+fi
+
+# Activate virtual environment if you have one
+# source venv/bin/activate
+
+# Run Django migrations
+python3.12 manage.py migrate
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cd /var/www/html/auditions
+
+# Navigate to the project directory
+PROJECT_DIR="/home/ec2-user/auditions"
+if [ -d "$PROJECT_DIR" ]; then
+    cd "$PROJECT_DIR"
+else
+    echo "Directory $PROJECT_DIR does not exist."
+    exit 1
+fi
+
 # Replace with your server start command, e.g., using Gunicorn
 gunicorn --workers 3 auditions.wsgi:application --bind 0.0.0.0:8000 --daemon
