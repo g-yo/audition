0acb2b3ed1d04c6c05438073da2e5ce2464d6941
g-yo <geoaugustion@gmail.com>
2024-08-08 11:35:22 +0530

    apply

--- movieaudition/settings.py
+++ movieaudition/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ddedbfe5627d6e4f9dcbb9ea1754f6ccdee11091476392e3b90543b2be6a54c
-size 3116
+oid sha256:a1dfa266d4cbaa3fcb27668eb3f645ef7ff99e35bc4f182dc3cc2d5a487e8a6b
+size 3120
