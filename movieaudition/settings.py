f2a360b0eb6867331ec25a54d42c2f3cf79f6c87
g-yo <geoaugustion@gmail.com>
2024-08-08 17:44:35 +0530

    apply

--- movieaudition/settings.py
+++ movieaudition/settings.py
@@ -35,7 +35,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 AUTH_USER_MODEL = 'auditions.CustomUser'
-#ks
+#ksfadf
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
