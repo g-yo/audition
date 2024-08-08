2a01788ee2d4feb99bb17060d18638a372f9075f
g-yo <geoaugustion@gmail.com>
2024-08-08 08:47:56 +0530

    apply

--- movieaudition/settings.py
+++ movieaudition/settings.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--l0(22d173e_70j#gkcv0r81w)f%u48)**byamgu464o0_lmub'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -15,24 +16,22 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'auditions',
     'django.contrib.sites',
+    'storages',
 ]
 
+AWS_ACCESS_KEY_ID = 'AKIAU6GDYCKFR7MLHJ5N'
+AWS_SECRET_ACCESS_KEY = 'FxQEAskbXHcOH8YY6CvnEhQBAhIlNa26QBxqVRZ6'
+AWS_STORAGE_BUCKET_NAME = 'movieaudtion'
+AWS_S3_REGION_NAME = 'us-east-1'  # e.g., 'us-east-1'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_CUSTOM_DOMAIN = 'movieaudtion.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # This can be removed if you're using S3 for media
+
 AUTH_USER_MODEL = 'auditions.CustomUser'
-
-
-FIREBASE_CREDENTIALS = {
-    'type': 'service_account',
-    'project_id': '<your-project-id>',
-    'private_key_id': '<your-private-key-id>',
-    'private_key': '<your-private-key>',
-    'client_email': '<your-client-email>',
-    'client_id': '<your-client-id>',
-    'auth_uri': '<your-auth-uri>',
-    'token_uri': '<your-token-uri>',
-    'auth_provider_x509_cert_url': '<your-auth-provider-cert-url>',
-    'client_x509_cert_url': '<your-client-cert-url>',
-}
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -66,8 +65,12 @@ WSGI_APPLICATION = 'movieaudition.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movieaudtions',
+        'USER': 'postgres',
+        'PASSWORD': 'eZiO8301',
+        'HOST': 'movieaudtions.chkka4ea4ew0.us-east-1.rds.amazonaws.com',  # e.g., 'your-db-instance.123456789012.us-east-1.rds.amazonaws.com'
+        'PORT': '5432',  # PostgreSQL default port
     }
 }
 
@@ -99,5 +102,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Optional: If you have static files locally
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- scripts/validate_service.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-# Add validation commands here, e.g., checking if the server is running
